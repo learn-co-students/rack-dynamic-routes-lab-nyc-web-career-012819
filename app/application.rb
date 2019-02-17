329afac77da45ca900ec4f1d0a32d89946d7f2b1
github username <github email address>
2019-02-16 22:04:43 -0500

    refactored

--- app/application.rb
+++ app/application.rb
@@ -9,7 +9,7 @@ class Application
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last
       item = @@items.find{|item| item.name == item_name}
-      if @@items.include?(item)
+      if item
         item_price = item.price
         resp.write "#{item_price}"
       else
